2390813331c582c6578be10236581f369b32f731
dw <gpmato10@gmail.com>
2016-01-18 05:56:41 +0900

    2.3.3 171p END.

--- src/test/java/springbook/user/dao/UserDaoTestkk.java
+++ src/test/java/springbook/user/dao/UserDaoTestkk.java
@@ -5,6 +5,9 @@ import org.junit.runner.JUnitCore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
@@ -16,22 +19,25 @@ public class UserDaoTestkk {
 
         UserDao dao = context.getBean("userDao3", UserDao.class);
 
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("leegw700", "이길원", "springno2");
+
+
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user = new User();
-        user.setId("gyumee");
-        user.setName("박성철");
-        user.setPassword("springno1");
+        dao.add(user1);
+        dao.add(user2);
 
-        dao.add(user);
-        assertThat(dao.getCount(), is(1));
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        User user2 = dao.get(user.getId());
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
-        System.out.println("ddd");
+        System.out.println("andAndGet Success!");
 
     }
 
@@ -39,15 +45,34 @@ public class UserDaoTestkk {
     public void testName() throws Exception {
         ApplicationContext context = new GenericXmlApplicationContext("c.xml");
         User us2 = context.getBean("user33", User.class);
-        us2.setId("d");
+        us2.setId("setId : hello");
 
         System.out.println(us2.getId());
-
-
-
     }
 
-    public static void main(String[] args) {
-        JUnitCore.main("springbook.user.dao.UserDaoTestkk");
+    @Test
+    public void count() throws SQLException, ClassNotFoundException {
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+
+        UserDao dao = context.getBean("userDao3", UserDao.class);
+
+        User user1 = new User("gyumee", "박성철", "springno1");
+        User user2 = new User("taehee", "김태희", "미모2");
+        User user3 = new User("jihyun", "전지현", "미모3");
+
+        dao.deleteAll();
+        assertThat(dao.getCount(), is(0));
+
+        dao.add(user1);
+        assertThat(dao.getCount(), is(1));
+
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
+
+        dao.add(user3);
+        assertThat(dao.getCount(), is(3));
+
+        System.out.println("count() Success!");
+
     }
 }
