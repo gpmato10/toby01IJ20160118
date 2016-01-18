a754d70fcdecba3c0c174318acede40dab27e94b
dw <gpmato10@gmail.com>
2016-01-19 03:51:14 +0900

    2.5.2 END 204p 까지.. 코드가 이해가 안되네

--- src/test/java/springbook/learningtest/junit/JUnitTest.java
+++ src/test/java/springbook/learningtest/junit/JUnitTest.java
@@ -1,28 +1,51 @@
 package springbook.learningtest.junit;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
-/**
- * Created by dw on 2016. 1. 18..
- */
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:/junit.xml")
 public class JUnitTest {
-    static JUnitTest testObject;
+
+    @Autowired
+    ApplicationContext context;
+
+    static Set<JUnitTest> testObjects = new HashSet<>();
+    static ApplicationContext contextObject = null;
 
     @Test
     public void test1() throws Exception {
-        assertThat(this, is(not(sameInstance(testObject))));
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
     }
 
     @Test
     public void test2() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(contextObject == null || contextObject == this.context, is(true));
+        contextObject = context;
+
+        assertTrue(contextObject == null || contextObject == this.context);
+        contextObject = this.context;
     }
 
     @Test
     public void test3() {
-        assertThat(this, is(not(sameInstance(testObject))));
-        testObject = this;
+        assertThat(contextObject, either(is(nullValue())).or(is(this.context)));
+        contextObject = this.context;
+    }
+
+    @Test
+    public void test4() {
+        System.out.println(not(hasItem(this)));
     }
 }
