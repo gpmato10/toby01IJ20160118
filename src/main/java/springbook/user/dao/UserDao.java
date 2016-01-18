411b54bb6a6db61154c9c4454e4e4639a121eb9c
dw <gpmato10@gmail.com>
2016-01-19 06:53:36 +0900

    3.4.2 JdbcContext 의 특별한 DI ( 코드를 이용한 수동 DI) p240 END. 리스트 3-25 : JdbcContext 생성과 이 작업을 수행하는 setDataSource() 메소드

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -13,15 +13,16 @@ public class UserDao {
 
     private DataSource dataSource;
 
+    private JdbcContext jdbcContext;
+
     public void setDataSource(DataSource dataSource) {
+        this.jdbcContext = new JdbcContext();
+        this.jdbcContext.setDataSource(dataSource);
         this.dataSource = dataSource;
     }
 
-    private JdbcContext jdbcContext;
 
-    public void setJdbcContext(JdbcContext jdbcContext) {
-        this.jdbcContext = jdbcContext;
-    }
+
 
     public void add(final User user) throws ClassNotFoundException, SQLException {
         this.jdbcContext.workWithStatementStrategy(new StatementStrategy() {
