c38221e0eca7fe7a36def0982dfa40be4b247e98
dw <gpmato10@gmail.com>
2016-01-19 04:41:01 +0900

    3.2.2 p223 END. 리스트 3-12 클라이언트 책임을 담당합 deleteAll() 메소드

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -65,9 +65,8 @@ public class UserDao {
         try {
             c = dataSource.getConnection();
 
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-            ps.executeUpdate();
+            StatementStrategy st = new DeleteAllStatement();
+            jdbcContextWithStatementStrategy(st);
         } catch (SQLException e) {
             throw e;
         } finally {
@@ -92,6 +91,23 @@ public class UserDao {
         PreparedStatement ps;
         ps = c.prepareStatement("delete from users");
         return ps;
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
+            if (ps != null) { try { ps.close();} catch (SQLException e) {} }
+            if (c != null) { try { c.close();} catch (SQLException e) {} }
+        }
     }
 
     public int getCount() throws SQLException {
