874543353f7386512067f1e37ac97db450173eff
dw <gpmato10@gmail.com>
2016-01-20 05:23:06 +0900

    3.5.3 p256 END. 리스트 3-40 lineReadTemplate()을 사용하도록 수정한 calSum(),  calcMultiply() 메소드

---
+++ src/main/java/springbook/user/dao/LineCallback.java
@@ -0,0 +1,8 @@
+package springbook.user.dao;
+
+/**
+ * Created by dw on 2016. 1. 20..
+ */
+public interface LineCallback {
+    Integer doSomethingWithLine(String line, Integer value);
+}
--- src/test/java/springbook/learningtest/template/Calculator.java
+++ src/test/java/springbook/learningtest/template/Calculator.java
@@ -1,6 +1,7 @@
 package springbook.learningtest.template;
 
 import org.hamcrest.Matcher;
+import springbook.user.dao.LineCallback;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
@@ -13,19 +14,14 @@ public class Calculator {
     BufferedReader br = null;
 
     public Integer calcSum(String filepath) throws IOException {
-        BufferedReaderCallBack sumCallback =
-                new BufferedReaderCallBack() {
+        LineCallback sumCallback =
+                new LineCallback() {
                     @Override
-                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                        Integer sum = 0;
-                        String line = null;
-                        while ((line = br.readLine()) != null) {
-                            sum += Integer.valueOf(line);
-                        }
-                        return sum;
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value + Integer.valueOf(line);
                     }
                 };
-        return fileReadTemplate(filepath, sumCallback);
+        return lineReadTemplate(filepath, sumCallback, 0);
     }
 
     public Integer fileReadTemplate(String filepath, BufferedReaderCallBack callBack) throws IOException {
@@ -48,18 +44,40 @@ public class Calculator {
     }
 
     public Integer calcMultiply(String filepath) throws IOException {
-        BufferedReaderCallBack multiplyCallback =
-                new BufferedReaderCallBack() {
+        LineCallback multiplyCallback =
+                new LineCallback() {
                     @Override
-                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
-                        Integer multiply = 1;
-                        String line = null;
-                        while ((line = br.readLine()) != null) {
-                            multiply *= Integer.valueOf(line);
-                        }
-                        return multiply;
+                    public Integer doSomethingWithLine(String line, Integer value) {
+                        return value * Integer.valueOf(line);
                     }
                 };
-        return fileReadTemplate(filepath, multiplyCallback);
+        return lineReadTemplate(filepath, multiplyCallback, 1);
+    }
+    public Integer lineReadTemplate(String filepath, LineCallback callback, int initVal)
+            throws IOException {
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(filepath));
+
+            Integer res = initVal;
+            String line = null;
+            while ((line = br.readLine()) != null) {
+                res = callback.doSomethingWithLine(line, res);
+            }
+            return res;
+        }
+        catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw e;
+        }
+        finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException e) {
+                    System.out.println(e.getMessage());
+                }
+            }
+        }
     }
 }
